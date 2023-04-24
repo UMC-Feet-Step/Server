9e250dbbb88766273e7567ba9ff4391f7ffe3bd1
이정우 <acg6138@naver.com>
2023-04-24 12:41:29 +0900

    [UPDATE] : 게시글 수정(GET Method)시 이미지도 반환하도록 재수정

--- src/main/java/com/footstep/domain/posting/dto/EditPostingDto.java
+++ src/main/java/com/footstep/domain/posting/dto/EditPostingDto.java
@@ -19,6 +19,8 @@ public class EditPostingDto {
     @ApiModelProperty(value = "발자취 게시일", required = true, example = "2023-01-23")
     private Date recordDate;
     private CreatePlaceDto createPlaceDto;
+    @ApiModelProperty(notes = "발자취 사진", example = "http://news.samsungdisplay.com/wp-content/uploads/2018/08/1.png")
+    private String imageUrl;
     @ApiModelProperty(value = "발자취 공개 여부", required = true, example = "1")
     private int visibilityStatusCode;
 }
